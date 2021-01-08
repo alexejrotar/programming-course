058ea98f190b98c9a86de2439e7f5f309195ea7c
alexejrotar <77157990+alexejrotar@users.noreply.github.com>
2021-01-08 16:12:34 +0100

    Add call to mean in index.html

--- exercises/mean.js
+++ exercises/mean.js
@@ -1,11 +1,11 @@
-let a = 5
-let b = 8
-let c = 10
+let a = 5;
+let b = 8;
+let c = 10;
 
-let mean = 0
+let mean = 0;
 
-const abcSpan = document.getElementById('abc')
-const meanSpan = document.getElementById('mean')
+const abcSpan = document.getElementById("abc");
+const meanSpan = document.getElementById("mean");
 
-abcSpan.innerHTML = `a=${a}, b=${b}, c=${c}`
-meanSpan.innerHTML = mean
+abcSpan.innerHTML = `${a}, ${b}, ${c}`;
+meanSpan.innerHTML = mean;
