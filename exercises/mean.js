86e5c169d698df3653ad860ea57625bd89d5e971
Alexej Rotar <alexejrotar@gmail.com>
2021-01-08 16:08:41 +0100

    Add mean.js

---
+++ exercises/mean.js
@@ -0,0 +1,11 @@
+let a = 5
+let b = 8
+let c = 10
+
+let mean = 0
+
+const abcSpan = document.getElementById('abc')
+const meanSpan = document.getElementById('mean')
+
+abcSpan.innerHTML = `a=${a}, b=${b}, c=${c}`
+meanSpan.innerHTML = mean
