28ff831c61d4719ef9735700089a4a47f0539b4a
Alexej Rotar <kontakt@alexejrotar.de>
2021-01-19 08:13:24 +0100

    Examples for operators and pqformula

--- src/pages/basics/exercises/operators.js
+++ src/pages/basics/exercises/operators.js
@@ -1,6 +1,10 @@
 {
   let a, output;
   a = 0;
-  output = `a = ${a}`;
+  let b = 2/2;
+  let c = (50-20)*6+(80/2);
+  let d = 2**3;
+  let e = 2**(1/2);
+  output = `b = ${b}`;
   appendTo('operators', output);
 }
--- src/pages/basics/exercises/pqformula.js
+++ src/pages/basics/exercises/pqformula.js
@@ -1,20 +1,33 @@
 {
-  let x1 = 0;
-  let x2 = 0;
+  // x^2 + 1/2x -6
+  let x1 = (-1/2+((1/2)**2-4*(-6))**(1/2))/2;
+  let x2 = (-1/2-((1/2)**2-4*(-6))**(1/2))/2;
+
+
+  let x3 = (1/2)**2 + 4*6;
+  x3 = x3**(1/2);
+  x3 -= 1/2;
+  x3 /= 2;
+
 
   let output = `<p>x1 = ${x1}, x2 = ${x2}</p>`;
   appendTo('pqform', output);
 
+  function roots(p, q) {
+    let x1 = (-p+(p**2-4*q)**(1/2))/2;
+    let x2 = (-p-(p**2-4*q)**(1/2))/2;
+    return [x1, x2];
+  }
 
-  [x1, x2] = [0,0];
+  [x1, x2] = roots(-1/3, -5);
   output = `<p>x1 = ${x1}, x2 = ${x2}</p>`;
   appendTo('pqfun-a', output);
 
-  [x1, x2] = [0,0];
+  [x1, x2] = roots(2, -10);
   output = `<p>x1 = ${x1}, x2 = ${x2}</p>`;
   appendTo('pqfun-b', output);
 
-  [x1, x2] = [0,0];
+  [x1, x2] = roots(-3, -8);
   output = `<p>x1 = ${x1}, x2 = ${x2}</p>`;
   appendTo('pqfun-c', output);
 
