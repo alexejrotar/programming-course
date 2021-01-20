c783bf84ace0871dc874cf1af586f4238bfd9139
Alexej Rotar <kontakt@alexejrotar.de>
2021-01-20 16:25:59 +0100

    add counter and swap exercises to functions

---
+++ src/pages/basics/exercises/counter.js
@@ -0,0 +1,10 @@
+{
+  let counter = 0;
+
+  // write your function here
+  
+  for (let i=0; i<=20; i++) {
+    let output = `${counter} `;
+    appendTo('counter', output);
+  }
+}
---
+++ src/pages/basics/exercises/swap.js
@@ -0,0 +1,10 @@
+{
+  let a = 5;
+  let b = 10;
+
+  // define and call your function here
+
+  // do not alter code below this comment
+  let output = `a = ${a}, b = ${b}`;
+  appendTo('swapfun', output);
+}
