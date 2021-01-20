d75f043cae11b84396d61a2561016489acee2472
Alexej Rotar <kontakt@alexejrotar.de>
2021-01-20 18:35:44 +0100

    Restructure basics directory

--- src/pages/basics/functions/pqfun.js
+++ src/pages/basics/functions/pqfun.js
@@ -2,10 +2,6 @@
   let x1 = 0;
   let x2 = 0;
 
-  let output = `<p>x1 = ${x1}, x2 = ${x2}</p>`;
-  appendTo('pqform', output);
-
-
   [x1, x2] = [0,0];
   output = `<p>x1 = ${x1}, x2 = ${x2}</p>`;
   appendTo('pqfun-a', output);
@@ -17,13 +13,4 @@
   [x1, x2] = [0,0];
   output = `<p>x1 = ${x1}, x2 = ${x2}</p>`;
   appendTo('pqfun-c', output);
-
-
-  [x1, x2] = [0,0];
-  output = `<p>x1 = ${x1}, x2 = ${x2}</p>`;
-  appendTo('pqfun-improved-a', output);
-
-  [x1, x2] = [0,0];
-  output = `<p>x1 = ${x1}, x2 = ${x2}</p>`;
-  appendTo('pqfun-improved-b', output);
 }
