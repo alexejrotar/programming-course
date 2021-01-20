63e8c25a00c6b42726335858aeedba48cbe85f73
Alexej Rotar <kontakt@alexejrotar.de>
2021-01-20 21:53:33 +0100

    prepare pqimproved

---
+++ src/pages/basics/branching/pqimproved.js
@@ -0,0 +1,14 @@
+{
+  // the function should return [x1, x2] in case of two roots (e.g. [1, 3])
+  // it should return x in case of one root (e.g. 5)
+  // and it should return 'no roots' in case of no roots
+  // your code here
+
+  let roots1 = 'no roots'; // call the function here
+  let output = `${roots1}`;
+  appendTo('pqfun-improved-a', output);
+
+  let roots2 = 0; // and here
+  output = `${roots2}`;
+  appendTo('pqfun-improved-b', output);
+}
