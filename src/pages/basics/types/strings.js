7d711d83995d330b7c9fae3f57d8e738cb005432
Alexej Rotar <kontakt@alexejrotar.de>
2021-02-25 13:30:08 +0100

    replace numbers with variables in strings exercise

--- src/pages/basics/types/strings.js
+++ src/pages/basics/types/strings.js
@@ -4,9 +4,11 @@ function strings() {
   appendParagraph('strings', "hello");
   appendParagraph('strings', 'world');
   appendParagraph('strings', "hello" + `world`);
-  appendParagraph('strings', "7 - 3 = 7 - 3");
-  appendParagraph('strings', `5 + 10 = ${5 + 10}`);
-  appendParagraph('strings', '5 * 2 = ${5 * 2}');
+  let a = 7;
+  let b = 3;
+  appendParagraph('strings', "7 - 3 = a - b");
+  appendParagraph('strings', `7 + 3 = ${a + b}`);
+  appendParagraph('strings', '7 * 3 = ${a * b}');
 }
 
 
