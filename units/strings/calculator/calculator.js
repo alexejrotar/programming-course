13483ae7a8797190a4389a9be0fc4b2b99964a57
Alexej Rotar <kontakt@alexejrotar.de>
2021-03-18 21:13:14 +0100

    finish calculator

--- units/strings/calculator/calculator.js
+++ units/strings/calculator/calculator.js
@@ -1,20 +1,28 @@
-function Basiccalculator() {
-  let basicvar = {
-    add: '+',
-    sub: '-',
-    div: '/',
-    mlt: '*',
-  }
-{
-    let a = parseFloat;
-    let b = parseFloat;
-    let result = 'a + b'
-}
+// computation = 4 + 3
+function Basiccalculator(computation) {
+  let split = computation.split(' '); // ["4", "+", "3"] 
+  let a = parseFloat(split[0]); // 4
+  let b = parseFloat(split[2]); // 3
 
+  let c = split[1]; // +
+  
+  if (c == '+') {
+    return a + b; 
+  }
+  else if (c == '-'){
+    return a - b;
+  }
+  else if (c == '/'){
+    return a / b;
+  }
+  else if (c == '*'){
+    return a * b;
+  }
+}
 
 function handleCompute() {
   let computation = document.forms.calculatorForm.elements.computation.value;
-  let result = blub(computation); // call your function here
+  let result = Basiccalculator(computation); // call your function here
 
   replaceHtml('computation', `<p>${result}</p>`);
 }
