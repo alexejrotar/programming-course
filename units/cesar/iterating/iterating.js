45dae44818c51267412ed80aa94a4c2f68de0084
Alexej Rotar <kontakt@alexejrotar.de>
2021-03-18 19:56:19 +0100

    restructure iterating functions

--- units/cesar/iterating/iterating.js
+++ units/cesar/iterating/iterating.js
@@ -1,18 +1,22 @@
 let counter = 0;
 
 function countFromCounter(to) {
-	appendTo('countFromCounter', `${counter} `);
 	if (counter == to) {
   	return;
 	}
+
+	appendTo('countFromCounter', `${counter} `);
+
 	counter = counter + 1;
 	countFromCounter(to);
 }
 
 function count(from, to) {
-  appendTo('count', `${from} `);
 	if (from == to) {
   	return;
 	}
+
+	appendTo('count', `${from} `);
+
 	count(from+1, to);
 }
