a5646b5890ba98abf67dbf97e94de4686fd5d73c
Alexej Rotar <kontakt@alexejrotar.de>
2021-03-25 20:41:28 +0100

    improve calculator

--- units/strings/calculator/calculator.js
+++ units/strings/calculator/calculator.js
@@ -1,22 +1,49 @@
+'use strict';
 // computation = 4 + 3
+let memory = 0;
+
 function Basiccalculator(computation) {
   let split = computation.split(' '); // ["4", "+", "3"] 
-  let a = parseFloat(split[0]); // 4
-  let b = parseFloat(split[2]); // 3
+  let a = split[0]; // 4
+  let b = split[2]; // 3
 
+  // TODO: strukturiere den Code durch Kommentare
   let c = split[1]; // +
-  
-  if (c == '+') {
-    return a + b; 
+  if (a && b && c) {
+    a = parseFloat(a);
+    b = parseFloat(b);
+    if (c == '+') {
+      memory = a + b;
+    }
+    else if (c == '-') {
+      memory = a - b;
+    }
+    else if (c == '/') {
+      memory = a / b;
+    }
+    else if (c == '*') {
+      memory = a * b;
+    }
+    return memory;
   }
-  else if (c == '-'){
-    return a - b;
+  else if (a && c) {
+    c = parseFloat(c);
+    if (a == '+') {
+      memory += c
+    }
+    if (a == '-') {
+      memory -= c
+    }
+    if (a == '/') {
+      memory /= c
+    }
+    if (a == '*') {
+      memory *= c
+    }
+    return memory;
   }
-  else if (c == '/'){
-    return a / b;
-  }
-  else if (c == '*'){
-    return a * b;
+  else {
+    return "wrong input";
   }
 }
 
