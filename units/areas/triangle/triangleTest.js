2546a114cc910aa2ae90f1040c47114fce770ea6
Alexej Rotar <kontakt@alexejrotar.de>
2021-03-31 16:08:04 +0200

    Add tests for triangle

---
+++ units/areas/triangle/triangleTest.js
@@ -0,0 +1,15 @@
+triangleTest = new Test('triangleTest');
+
+triangleTest.test('Triangle Area', () => {
+	expected = 10;
+	actual = triangleArea(5,4);
+	assertSame(expected, actual);
+});
+
+triangleTest.test('Rectangular Triangle Height', () => {
+
+});
+
+triangleTest.test('Rectangular Triangle Area', () => {
+
+});
