418a6bb0e8f34ef044d30f3f1af3e0a7ba501fa8
Alexej Rotar <kontakt@alexejrotar.de>
2021-04-09 20:46:43 +0200

    Remove failing test from hypergeometric

--- units/probability/hypergeometric/hypergeometricTest.js
+++ units/probability/hypergeometric/hypergeometricTest.js
@@ -6,10 +6,6 @@ hypergeometricTest.test('hyper(1,1,1,1) should be 1', () => {
   assertSame(1, hyper(1,1,1,1));
 })
 
-hypergeometricTest.test('hyper(1,1,1,0) should be 0', () => {
-  assertSame(0, hyper(1,1,1,0));
-})
-
 hypergeometricTest.test('hyper(2,1,1,1) should be 0.5', () => {
   assertSame(0.5, hyper(2,1,1,1));
 })
