52d7535076e4dfb312e018ca3506aa4d9f5b01b8
Alexej Rotar <kontakt@alexejrotar.de>
2021-04-29 17:54:08 +0200

    Add loop versions for cesar

--- units/cesar/encryption/encryption.js
+++ units/cesar/encryption/encryption.js
@@ -10,9 +10,31 @@ function encryptAux (text, x, index) {
     return '';
   }
 
-  let result = encryptSingle (text[index], x)
+  let result = encryptSingle (text[index], x);
 
-return result + encryptAux (text, x, index+1)
+return result + encryptAux (text, x, index+1);
+}
+
+function encryptWhile(text, x) {
+  let index = 0;
+  let result = '';
+
+  while(text.length != index) {
+    result += encryptSingle(text[index], x);
+    index += 1;
+  }
+
+  return result;
+}
+
+function encryptFor(text, x) {
+  let result = '';
+
+  for(let index = 0; index < text.length; index += 1) {
+    result += encryptSingle(text[index], x);
+  }
+
+  return result;
 }
 
 
@@ -20,5 +42,8 @@ function handleEncrypt() {
   const plain = document.forms.encryptionForm.elements.plainText.value;
   const key = parseInt(document.forms.encryptionForm.elements.key.value);
   const encrypted = encrypt(plain, key);
+  //const encrypted = encryptWhile(plain, key);
+  //const encrypted = encryptFor(plain, key);
+
   replaceHtml('encryption', `<p>${encrypted}</p>`);
 }
