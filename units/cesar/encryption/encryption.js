fc0f6a46b90c0eaa4b04439863a0c9b2f7d0f810
Alexej Rotar <kontakt@alexejrotar.de>
2021-04-29 17:12:17 +0200

    Solve cesar

--- units/cesar/encryption/encryption.js
+++ units/cesar/encryption/encryption.js
@@ -1,12 +1,24 @@
 'use strict';
 
-function encrypt(plain, key) {
-  return plain;
+
+function encrypt(text, x) {
+  return encryptAux(text, x, 0);
 }
+
+function encryptAux (text, x, index) {
+  if (text.length == index) {
+    return '';
+  }
+
+  let result = encryptSingle (text[index], x)
+
+return result + encryptAux (text, x, index+1)
+}
+
 
 function handleEncrypt() {
   const plain = document.forms.encryptionForm.elements.plainText.value;
-  const key = document.forms.encryptionForm.elements.key.value;
+  const key = parseInt(document.forms.encryptionForm.elements.key.value);
   const encrypted = encrypt(plain, key);
   replaceHtml('encryption', `<p>${encrypted}</p>`);
 }
--- units/cesar/singleCharEncryption/singleCharEncryption.js
+++ units/cesar/singleCharEncryption/singleCharEncryption.js
@@ -1,1 +1,12 @@
 'use strict';
+
+function encryptSingle (a, x) {
+
+    let memory = a.charCodeAt(0);
+    let encryption = memory + x;
+    if (encryption >= 123) {
+      encryption = encryption - 26;
+    }
+
+    return String.fromCharCode(encryption);
+}
